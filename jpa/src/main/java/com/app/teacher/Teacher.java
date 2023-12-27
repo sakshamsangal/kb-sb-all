e282da6649841035ca81443e32f45c36e51725fc
saksham <sakshamsangal111@gmail.com>
2023-12-27 22:43:51 +0530

    up

--- jpa/src/main/java/com/app/teacher/Teacher.java
+++ jpa/src/main/java/com/app/teacher/Teacher.java
@@ -1,7 +1,6 @@
 package com.app.teacher;
 
 import com.app.course.Course;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +10,22 @@ import lombok.Setter;
 import java.util.Set;
 
 
-
+//curl --location --request GET 'http://localhost:8080/teacher/course' \
+//--header 'Content-Type: application/json' \
+//--header 'Authorization: Basic YWRtaW4tY2xpZW50OnBhc3M=' \
+//--data '{
+//    "name": "sak1",
+//    "age": 12,
+//    "dept": "CS",
+//    "courses": [
+//        {
+//            "title": "t2",
+//            "abbreviation":"abr",
+//            "modules": 12,
+//            "fee":123.2
+//        }
+//    ]
+//}'
 @Entity
 @Table(name = "TEACHER_TBL")
 @Setter
@@ -39,7 +53,6 @@ public class Teacher {
                     @JoinColumn(name = "course_id", referencedColumnName = "id")
             }
     )
-    @JsonManagedReference
     private Set<Course> courses;
 
 }
